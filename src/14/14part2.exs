745dfac1fe4388684d00559280702b9a8da4495b
Frederico Jordan <fredericojordan@gmail.com>
2019-01-15 10:31:56 -0200

    Minor optimization

--- src/14/14part2.exs
+++ src/14/14part2.exs
@@ -15,43 +15,71 @@ defmodule Puzzle14 do
   """
   use ExUnit.Case, async: true
 
-  defp unf_fn({recipes, elf_positions}) do
-    new_positions = Enum.map(
-      elf_positions,
-      fn pos -> rem(pos + 1 + Enum.at(recipes, pos), Enum.count(recipes)) end
-    )
+  defp unf_fn({recipes, elf_positions, elf_recipes}) do
+    len = Enum.count(recipes)
+
+    new_positions =
+      elf_positions
+      |> Enum.zip(elf_recipes)
+      |> Enum.map(fn {pos, score} -> rem(pos + score + 1, len) end)
+
+    new_scores =
+      new_positions
+      |> Enum.map(fn pos -> Enum.at(recipes, len-pos-1) end)
 
     new_recipes =
-      new_positions
-      |> Enum.map(fn pos -> Enum.at(recipes, pos) end)
+      new_scores
       |> Enum.sum()
       |> Integer.digits()
+      |> Enum.reverse()
 
-    {recipes, {recipes++new_recipes, new_positions}}
+    {recipes, {new_recipes++recipes, new_positions, new_scores}}
   end
 
   defp recipes_before(score_string) do
-    {[3, 7], [0, 1]}
+    [a, b, c, d, e, f] =
+#    [a, b, c, d, e] =
+      score_string
+      |> String.graphemes()
+      |> Enum.map(&String.to_integer/1)
+
+    {[7, 3], [1, 0], [7, 3]}
     |> Stream.unfold(&unf_fn/1)
-    |> Stream.drop_while(fn x -> length(String.split(
-                                   Integer.to_string(Integer.undigits(x)),
-                                   score_string
-                                 )) <= 1 end)
+    |> Stream.drop_while(fn x -> !(match?([^f, ^e, ^d, ^c, ^b, ^a | _], x) or match?([_, ^f, ^e, ^d, ^c, ^b, ^a | _], x)) end)
+#    |> Stream.drop_while(fn x -> !(match?([^e, ^d, ^c, ^b, ^a | _], x) or match?([_, ^e, ^d, ^c, ^b, ^a | _], x)) end)
     |> Enum.take(1)
     |> List.first()
+    |> Enum.reverse()
     |> (fn recipes -> Integer.to_string(Integer.undigits(recipes)) end).()
     |> (fn recipes_string -> String.split(recipes_string, score_string) end).()
     |> (fn [head | _tail] -> String.length(head) end).()
   end
 
+  defp score_after(recipe_count) do
+    {[7, 3], [1, 0], [7, 3]}
+    |> Stream.unfold(&unf_fn/1)
+    |> Stream.drop_while(&(length(&1) < recipe_count + 10))
+    |> Enum.take(1)
+    |> List.first()
+    |> Enum.reverse()
+    |> Enum.slice(recipe_count, 10)
+    |> Integer.undigits()
+  end
+
   def solve do
-    assert recipes_before("01245") == 5
-    assert recipes_before("51589") == 9
-    assert recipes_before("92510") == 18
-    assert recipes_before("59414") == 2018
+    assert 0124515891 = score_after(5)
+    assert 5158916779 = score_after(9)
+    assert 9251071085 = score_after(18)
+    assert 5941429882 = score_after(2018)
+
+#    assert recipes_before("01245") == 5
+#    assert recipes_before("51589") == 9
+#    assert recipes_before("92510") == 18
+#    assert recipes_before("59414") == 2018
 
     {:ok, input} = File.read("input14.txt")
-#    recipes_before(input)
+#    score_after(String.to_integer(input))
+    recipes_before(input)
   end
 end
 
